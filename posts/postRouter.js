9b0ca3d29a9a431159e9dfc7475042815f0270ce
Antony Nyagah <antonynyagah@antonys-MacBook-Pro.local>
2019-05-15 20:04:18 -0500

    posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,54 @@
-const express = 'express';
+const express = require('express')
+const router = express.Router()
+const db_posts = require('./postDb')
 
-const router = express.Router();
+const middlewares = require('../middleware')
 
-router.get('/', (req, res) => {
+//READ
+router.get('/', async (req, res) => {
+    try {
+        const posts = await db_posts.get()
+        posts.length > 0
+        ?   res.status(200).json(posts)
+        :   res.status(400).json({message: `No posts present`})
+    }
+    catch (err) {
+        res.status(500).json({message: `Error: cannot GET posts!`})
+    }
+})
 
-});
+router.get('/:id', middlewares.validatepostid, async (req, res) => {
+    try {
+        
+        res.status(200).json(await db_posts.getById(req.params.id))
+    }
+    catch (err) {
+        res.status(500).json({message: `Error: cannot GET posts!`})
+    }
+})
 
-router.get('/:id', (req, res) => {
+//PUT
+router.put('/:id', middlewares.validatepostid, async (req, res) => {
+    try {
+        await db_posts.update(req.params.id, req.body)
+        ?   res.status(200).json({id: req.params.id, ...req.body})
+        :   res.status(404).json({message: `No posts present!`})
+    }
+    catch (err) {
+        res.status(500).json({error: `Error!`})
+    }
+})
 
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-// custom middleware
-
-function validatePostId(req, res, next) {
-
-};
+//DELETE
+router.delete('/:id', middlewares.validatepostid, async (req, res) => {
+    try {
+        await db_posts.remove(req.params.id)
+        ?   res.status(200).json({message: `Post deleted!`})
+        :   res.status(404).json({message: `Error: Invalid post id!`})
+    }
+    catch (err) {
+        res.status(500).json({error: `Error!`})
+    }
+})
 
 module.exports = router;
